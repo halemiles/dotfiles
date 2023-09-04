f0a87ddad7ae72b5950505c0de06d76e2b55aa87
Miles <mhale@protonmail.com>
2023-09-04 08:32:47 +0100

    Updated load files script to include more information. Updated background locations

--- load-dotfiles.sh
+++ load-dotfiles.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+echo "Loading dotfiles"
+
+echo "Copying i3 configs..."
 # i3
 cp -r ~/.config/i3 ~/.config/i3-backup
 cp -r i3 ~/.config
 
+echo "Copying nbim configs..."
 # Neovim
 cp -r ~/.config/nvim ~/.config/nvim-backup
 cp -r nvim ~/.config
 
+echo "Copying wallpaper..."
+# Wallpaper
+cp wallpaper/wallpaper.jpeg ~/Pictures/Wallpaper
+
+echo "Restarting i3..."
 i3 restart
+
+echo "Finished loading dotfiles"
